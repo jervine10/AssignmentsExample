142ab35768ea69b283467178c470c8b41c74717f
Josh Ervine <Josh.Ervine@accuweather.com>
2017-12-16 19:29:07 -0500

    Initial Commit

---
+++ TableViews/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TableViews
+//
+//  Created by Josh Ervine on 12/16/17.
+//  Copyright © 2017 Josh Ervine. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
